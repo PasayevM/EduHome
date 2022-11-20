0317ec15a271c23c36b0dac749b9e901a6e8c79e
PasayevM <mansurmurad305@gmail.com>
2022-11-20 06:13:48 +0400

    Init

---
+++ EduHome2/EduHome2/Controllers/CoursesController.cs
@@ -0,0 +1,25 @@
+﻿using EduHome2.DAL;
+using EduHome2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EduHome2.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CoursesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Course> courses = _db.Courses.OrderByDescending(x=>x.Id).ToList();
+            return View(courses);
+        }
+    }
+}
